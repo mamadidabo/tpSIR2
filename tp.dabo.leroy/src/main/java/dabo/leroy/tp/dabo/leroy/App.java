e6edee96aff06f2ab54b7cf76577b1b913c231d7
mamadidabo <mamadidabo@hotmail.fr>
2014-01-22 18:10:41 +0100

    initial

---
+++ tp.dabo.leroy/src/main/java/dabo/leroy/tp/dabo/leroy/App.java
@@ -0,0 +1,13 @@
+package dabo.leroy.tp.dabo.leroy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
