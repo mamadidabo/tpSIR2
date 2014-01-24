69a528aa9a5964b03f042704c5da3fd666f13631
mamadidabo <mamadidabo@hotmail.fr>
2014-01-24 17:44:07 +0100

    le prem

---
+++ tp1.dabo.leroy/src/main/java/tp1/dabo/leroy/tp1/dabo/leroy/App.java
@@ -0,0 +1,13 @@
+package tp1.dabo.leroy.tp1.dabo.leroy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
